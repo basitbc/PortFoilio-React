6662a43ffef1e1a61851518b0f522c41c5e33741
Basit Channa <bbchanna@gmail.com>
2022-11-01 22:52:35 +0530

    Some more Changes

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -1,9 +1,30 @@
-import { Grid } from '@mui/material'
-import React from 'react'
+import { Grid, Typography } from '@mui/material';
+import React from 'react';
+import "./styles.css";
 
 const About = () => {
   return (
-    <Grid display={"flex"}>About</Grid>
+    <Grid className="main-cont2" container height="100vh" width="100vw"  sx={{padding:{xs:"70px 20px",md:" 120px 165px"}}}>
+      <Grid container className="inside-cont2">
+        <Grid item className='in-text21'>
+        <hr
+        style={{
+            height: 2,
+            backgroundColor: "black",
+            width:"5%"
+        }}
+    />
+          <Typography paddingLeft={"25px"} sx={{fontSize:"10px", lineHeight:"10px", letterSpacing:"1px", fontFamily:"'Poppins', sans-serif"}}>SOME INFO</Typography> 
+        </Grid>
+        <Grid item className='in-text22'><Typography sx={{fontSize:"25.6px", letterSpacing:"2px",fontWeight:500, fontFamily:"'Poppins', sans-serif"}} >ABOUT ME</Typography></Grid>
+        <Grid container className='skill-in-cont2' sx={{padding:"18px 48px"}}>
+          <Typography className='text-in-disc' sx={{fontSize:"20px", fontWeight:"500", fontFamily:"'Poppins', sans-serif"}}>I'm a front-end web developer with a background in computer systems and network infrastructure. My 8 years of IT experience has given me a strong foundation for web development and building complex solutions. Recently, I graduated from the Juno College Immersive Web Development Bootcamp. I am passionate about coding and solving problems through code, and I am excited to work alongside other amazing programmers and learn so much more!</Typography>
+    
+        </Grid>
+    </Grid>
+    <Grid position={"relative"} height={"100px"} width="120vw" sx={{display:{xs:"flex", md:"none"}}}>
+      </Grid>
+  </Grid>
   )
 }
 
--- src/Pages/Contact/index.jsx
+++ src/Pages/Contact/index.jsx
@@ -1,8 +1,109 @@
-import React from 'react'
+import {  FormControl, Grid, TextField, Typography } from '@mui/material';
+import "./styles.css";
+import React, { useState } from 'react';
+import send, { sendForm } from 'emailjs-com';
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    to_name: '',
+    message: '',
+    reply_to: '',
+  });
+
+
+const handleSubmit = (e) => {
+  sendForm(
+    'service_s8wxwqg',
+    'template_vsb9udf',
+    toSend,
+    'BcGNB127J7CGrvLUa'
+  )
+    .then((response) => {
+      console.log('SUCCESS!', response.status, response.text);
+    })
+    .catch((err) => {
+      console.log('FAILED...', err);
+    });
+};
+
   return (
-    <div>Contact</div>
+    <Grid className="main-cont3" container height="100vh" width="100vw"  sx={{padding:{xs:"70px 20px",md:" 120px 165px"}}}>
+      <Grid container className="inside-cont3">
+        <Grid item className='in-text31'>
+        <hr
+        style={{
+            height: 2,
+            backgroundColor: "black",
+            width:"5%"
+        }}
+    />
+          <Typography paddingLeft={"25px"} sx={{fontSize:"10px", lineHeight:"10px", letterSpacing:"1px", fontFamily:"'Poppins', sans-serif"}}>SAY HELLO</Typography> 
+        </Grid>
+        <Grid item className='in-text32'><Typography sx={{fontSize:"25.6px", letterSpacing:"2px",fontWeight:500, fontFamily:"'Poppins', sans-serif"}} >CONTACT</Typography></Grid>
+        <Grid container className='skill-in-cont3' sx={{padding:"18px 48px"}}>
+         <Typography color={"#8444DF"} fontSize="20px" fontWeight={"600"} marginBottom="30px" textTransform={"uppercase"}>Get in Touch</Typography>
+         <Grid container rowGap={2}  className='form1' style={{justifyContent:"space-between"}}>
+      
+            <Grid xs={12} md={5.5}>
+            <TextField
+            fullWidth
+          required
+          id="outlined-required"
+          onChange={(e)=>{setName(e.target.value)}}
+          label="Name"
+          placeholder='Your Name' 
+        />
+            </Grid>
+            <Grid xs={12} md={5.5}>
+            <TextField
+            fullWidth
+          required
+          onChange={(e)=>{setEmail(e.target.value)}}
+          id="outlined-required"
+          label="Email"
+          placeholder='Your Email'
+        />
+        
+            </Grid>
+            <Grid xs={12}>
+            <TextField
+            fullWidth
+          required
+          onChange={(e)=>{setSubject(e.target.value)}}
+          id="outlined-required"
+          label="Subject"
+          placeholder='Subject'
+         
+        />
+            </Grid>
+
+            <Grid xs={12}>
+            <TextField
+            fullWidth
+          id="outlined-multiline-static"
+          onChange={(e)=>{setMessage(e.target.value)}}
+          required
+          label="Message"
+          multiline
+          rows={4}
+          placeholder="Write Your Message..."
+        />
+            </Grid>
+
+         </Grid>
+         <Grid>
+         <button onClick={()=>{handleSubmit()}} className='button'>Send</button>
+         </Grid>
+        </Grid>
+    </Grid>
+    <Grid position={"relative"} height={"100px"} width="120vw" sx={{display:{xs:"flex", md:"none"}}}>
+      </Grid>
+  </Grid>
   )
 }
 
